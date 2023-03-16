7389f782ae5e1f1dbf355b2d12bf5fea56ef41dc
Gareth Pledger <garpled@chbc.school.nz>
2023-03-17 10:31:45 +1300

    Add project files.

---
+++ ChemicalApp Gareth/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChemicalApp_Gareth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
