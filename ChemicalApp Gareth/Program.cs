f2a61ba72494d695b35533df2af735d445aa52cb
Gareth Pledger <garpled@chbc.school.nz>
2023-03-21 12:40:05 +1300

    Finished coding Global Variablse and added code to check if chemical hes been tested

--- ChemicalApp Gareth/Program.cs
+++ ChemicalApp Gareth/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ChemicalApp_Gareth
 {
     class Program
     {
+        //Gobal Variables
+
+        static List<string> chemicalNames = new List<string>();
+        static List<decimal> chemicalEfficiencies = new List<decimal>();
+
+        //Methods
+
+        static string FindChemical()
+        {
+            //checking if chemical has been tested, asking for chemical 
+
+            {
+                while (true)
+                {
+
+
+
+                    Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n" +
+                                        " Welcome to the testing phase. Please enter your chemical name\n" +
+                                        "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+
+                    string chemicalName = Console.ReadLine();
+
+                }
+            }
+        }
+
+        //Main Process/When Run
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+
+
+            //Creating efficiency rating
+
+
+
+            //Menu(useability)
         }
     }
 }
